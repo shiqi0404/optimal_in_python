81cdd242fed03cba28dea6152f11e70428b44adc
zou <zoushiqi0404@gmail.com>
2020-11-29 22:35:03 +0800

    Update demo2_14.py

--- demo2_14.py
+++ demo2_14.py
@@ -3,7 +3,11 @@
 -------------------------------------------------
 Project Name:   optimal_in_python 
 File Name：     demo2_14
-Description :   
+Description :   Pandas 基础
+                Numpy 是科学计算的核心，Pandas 是 Python 数据分析事实上的标准；
+                Pandas 的核心也是 Numpy，Pandas 是在 Numpy 的基础上封装了高级接口。
+                Pandas 提供了 Series 和 DataFrame 两种基础数据结构，
+                Series 表示序列数据；DataFrame 表示表格数据，且是由多个 Series 组成的。
 Author :        Steven.zou
 E-mail:         zoushiqi0404@gmail.com
 Date：          2020/11/29 下午9:59
@@ -13,3 +17,5 @@ Software:       PyCharm
                    2020/11/29:
 -------------------------------------------------
 """
+# Series
+
