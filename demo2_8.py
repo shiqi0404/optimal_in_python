ed006913b4b31720e8c10cfdf182cca4252fb980
邹世琦 <zoushiqi0404@gmail.com>
2021-10-27 13:12:22 +0800

    Default Changelist

--- demo2_8.py
+++ demo2_8.py
@@ -17,17 +17,19 @@ Software:       PyCharm
 # 集合迭代
 a = [1, 2, 3, 4, 5]
 
+
 def my_fun(x):
     print('do something on ', x)
     return x + 1
 
+
 # 迭代版本
 b = [my_fun(x) for x in a]
-print(b)    # 输出：[2, 3, 4, 5, 6]
+print(b)  # 输出：[2, 3, 4, 5, 6]
 
 # 循环版本
 b2 = []
 for x in a:
     t = x + 1
     b2.append(t)
-print(b2)   # 输出：[2, 3, 4, 5, 6]
+print(b2)  # 输出：[2, 3, 4, 5, 6]
