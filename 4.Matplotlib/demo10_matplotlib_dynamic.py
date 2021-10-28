f86d40a228081d7c79b87f20c65480c61c6a3428
邹世琦 <zoushiqi0404@gmail.com>
2021-10-28 22:42:49 +0800

    learn for《Python 最优化算法实战》

---
+++ 4.Matplotlib/demo10_matplotlib_dynamic.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+Project Name:   optimal_in_python 
+File Name：     demo10_matplotlib_dynamic
+Description :   绘制动态图
+Author :        Steven.zou
+E-mail:         zoushiqi0404@gmail.com
+Date：          2021-10-28 19:42
+Software:       PyCharm
+-------------------------------------------------
+ Change Activity:
+ 2021-10-28:
+-------------------------------------------------
+"""
