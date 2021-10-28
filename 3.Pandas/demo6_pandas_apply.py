0941c295b9ccf45aed7382ea133af93d15fb1224
邹世琦 <zoushiqi0404@gmail.com>
2021-10-28 18:34:24 +0800

    learn for《Python 最优化算法实战》

---
+++ 3.Pandas/demo6_pandas_apply.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+Project Name:   optimal_in_python 
+File Name：     demo6_pandas_apply
+Description :   apply函数 对目标集合中的每个元素执行相同的操作
+Author :        Steven.zou
+E-mail:         zoushiqi0404@gmail.com
+Date：          2021-10-28 18:33
+Software:       PyCharm
+-------------------------------------------------
+ Change Activity:
+ 2021-10-28:
+-------------------------------------------------
+"""
