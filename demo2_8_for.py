1c4f36a8da43cfb7924e483720a3d0696f069cf0
邹世琦 <zoushiqi0404@gmail.com>
2021-10-28 12:05:23 +0800

    Demo《Python 最优化算法实战》

--- demo2_8_for.py
+++ demo2_8_for.py
@@ -33,3 +33,25 @@ for x in a:
     t = x + 1
     b2.append(t)
 print(b2)  # 输出：[2, 3, 4, 5, 6]
+
+#  条件判断
+a = [1, 2, 3]
+if 1 in a:
+    print('元素1包含在列表a中')
+else:
+    print('元素1不包含在列表a中')
+
+# 循环控制
+# 1. 对某个数据集合进行迭代，满足条件时跳出循环
+b = [1, 2, 3, 4]
+for i in b:
+    if i > 3:
+        break   # 满足条件时跳出循环
+    print(i)
+
+# 2. 通过循环知道满足条件
+c = 1
+while c < 10:
+    c = c + 1
+    print('%d do something also.' % (c - 1))
+print(c)
