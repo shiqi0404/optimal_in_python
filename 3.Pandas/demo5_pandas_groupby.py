394b48187d8fa543f6c600c60cf1131176212c49
邹世琦 <zoushiqi0404@gmail.com>
2021-10-28 18:06:22 +0800

    learn for《Python 最优化算法实战》

--- 3.Pandas/demo5_pandas_groupby.py
+++ 3.Pandas/demo5_pandas_groupby.py
@@ -3,7 +3,7 @@
 -------------------------------------------------
 Project Name:   optimal_in_python 
 File Name：     demo5_pandas_groupby
-Description :   分组统计
+Description :   分组统计 应用 groupby 函数进行分组统计
 Author :        Steven.zou
 E-mail:         zoushiqi0404@gmail.com
 Date：          2021-10-28 17:59
@@ -13,3 +13,16 @@ Software:       PyCharm
  2021-10-28:
 -------------------------------------------------
 """
+
+import pandas as pd
+import numpy as np
+
+data = {
+    '学号': ['x1', 'x2', 'x3', 'x4', 'x5'],
+    '姓名': ['张三', '李四', '王五', '韩六', '赵七'],
+    '身高': [177, 151, 167, 175, 153],
+    '语文成绩': [92, 84, 80, 77, 87],
+    '学分': [1.5, 2.3, 3.2, 1.2, 1.8],
+    '日期': ['2019/3/23', '2019/3/24', '2019/3/25', '2019/3/26', '2019/3/27']
+}
+df = pd.DataFrame(data)
