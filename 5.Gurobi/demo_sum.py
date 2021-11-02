f8b6c1e7aa83f4c24f11109226afb86e1f95e7f7
邹世琦 <zoushiqi0404@gmail.com>
2021-11-02 17:58:52 +0800

    learn for《Python 最优化算法实战》

---
+++ 5.Gurobi/demo_sum.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+Project Name:   optimal_in_python 
+File Name：     demo_sum
+Description :   
+Author :        Steven.zou
+E-mail:         zoushiqi0404@gmail.com
+Date：          2021-11-02 17:58
+Software:       PyCharm
+-------------------------------------------------
+ Change Activity:
+ 2021-11-02:
+-------------------------------------------------
+"""
+'''
+具体code 可以参考Gurobi安装目录下 example 文件夹中示例
+'''
